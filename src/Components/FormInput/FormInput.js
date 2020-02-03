9345de03e1d374ec419b688082a958ff58c05b77
Anonymous <user@rivne.itstep.org>
2020-02-03 19:38:17 +0200

    deleted pass

--- src/Components/FormInput/FormInput.js
+++ src/Components/FormInput/FormInput.js
@@ -5,7 +5,6 @@ const FormInput = ({ handleChange, label, ...otherProps }) => {
   return (
     <div className="group">
       <input className="form-input" onChange={handleChange} {...otherProps} />
-
       {label ? (
         <label
           className={`${
