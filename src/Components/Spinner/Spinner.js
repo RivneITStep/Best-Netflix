a2b9098dc788d1e999dc221975ddcce3f554361f
Anonymous <user@rivne.itstep.org>
2020-02-03 19:44:23 +0200

    added spinner.scss

--- src/Components/Spinner/Spinner.js
+++ src/Components/Spinner/Spinner.js
@@ -4,7 +4,8 @@ import "./Spinner.scss";
 const Spinner = () => {
   return (
     <div className="spinner-overlay">
-      <div className="spinner-container"></div>
+      <div className="spinner-container">
+      </div>
     </div>
   );
 };
